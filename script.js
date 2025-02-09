3887378b4a1b585380c1935a5bf986d1b1693e29
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-09 12:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const outputTable = document.getElementById("output");
-    
+    cy.get("tbody#output").then(($tbody) => {
+  console.log($tbody.html()); // Check if "loading" is inside
+});
     // Initially display Loading row
     outputTable.innerHTML = `<tr><td colspan="2">Loading...</td></tr>`;
     
